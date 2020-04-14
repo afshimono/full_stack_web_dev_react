592d01bd422c33a628ca75ec40a7fa2441d9d156
Alexandre Shimono <alexandre@shimono.com.br>
2020-04-14 19:01:17 -0300

    Single Page App part 2

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -6,6 +6,7 @@ import {LEADERS} from '../shared/leaders'
 import {PROMOTIONS} from '../shared/promotions'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import DishDetail from './DishdetailComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent'
 import { Switch, Route, Redirect } from 'react-router-dom';
@@ -37,6 +38,13 @@ class Main  extends Component{
             />
         );
       }
+
+    const DishWithId = ({match}) => {
+    return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+    );
+    };
   
     return (
         <div className="App">
@@ -45,6 +53,7 @@ class Main  extends Component{
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route exact path='/contactus' component={Contact} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
