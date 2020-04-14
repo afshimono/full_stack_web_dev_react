881e5dc440135f74434665b58a5f5699aca15663
Alexandre Shimono <alexandre@shimono.com.br>
2020-04-14 11:03:13 -0300

    Single Page App part 1

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent'
 import {DISHES} from '../shared/dishes'
-import DishDetail from './DishdetailComponent'
+import {COMMENTS} from '../shared/comments'
+import {LEADERS} from '../shared/leaders'
+import {PROMOTIONS} from '../shared/promotions'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent'
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -14,6 +17,9 @@ class Main  extends Component{
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         }  
     }
 
@@ -25,6 +31,9 @@ class Main  extends Component{
     const HomePage = () => {
         return(
             <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
             />
         );
       }
@@ -32,8 +41,12 @@ class Main  extends Component{
     return (
         <div className="App">
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />
+              <Redirect to="/home" />
+          </Switch>
         <Footer />
         </div>
     );
