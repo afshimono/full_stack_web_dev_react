c34101b65012f724045385e8af6ccce507c5cf24
Alexandre Shimono <ashimono@gmail.com>
2020-08-04 09:03:33 -0300

    Express REST API with MongoDB and Mongoose Part 1

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -10,6 +10,20 @@ var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 
+const mongoose = require("mongoose");
+const Dishes = require("./models/dishes");
+const url = "mongodb+srv://<Login>:<Passowrd>@sandbox.oxqrh.gcp.mongodb.net/coursera";
+const connect = mongoose.connect(url);
+
+connect.then(
+	(db) => {
+		console.log("Connected correctly to server");
+	},
+	(err) => {
+		console.log(err);
+	}
+);
+
 var app = express();
 
 // view engine setup
@@ -30,18 +44,18 @@ app.use("/promotions", promoRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
