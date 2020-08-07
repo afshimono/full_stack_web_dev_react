f220319adc56ab3d93330749b0d8953de19a2578
Alexandre Shimono <alexandre@shimono.com.br>
2020-08-07 03:42:01 -0300

    Assignment 2!

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -12,16 +13,21 @@ var leaderRouter = require("./routes/leaderRouter");
 
 const mongoose = require("mongoose");
 const Dishes = require("./models/dishes");
-const url = "mongodb+srv://<Login>:<Passowrd>@sandbox.oxqrh.gcp.mongodb.net/coursera";
+const url =
+    "mongodb+srv://" +
+    process.env.MONGO_LOGIN +
+    ":" +
+    process.env.MONGO_PWD +
+    "@sandbox.oxqrh.gcp.mongodb.net/coursera";
 const connect = mongoose.connect(url);
 
 connect.then(
-	(db) => {
-		console.log("Connected correctly to server");
-	},
-	(err) => {
-		console.log(err);
-	}
+    (db) => {
+        console.log("Connected correctly to server");
+    },
+    (err) => {
+        console.log(err);
+    }
 );
 
 var app = express();
@@ -44,18 +50,18 @@ app.use("/promotions", promoRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
