a36a0912a4013f3f72ea7992017c9c2a1321a65f
Alexandre Shimono <alexandre@shimono.com.br>
2020-08-10 06:54:17 -0300

    Passport

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -13,6 +13,9 @@ var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 
+var passport = require("passport");
+var authenticate = require("./authenticate");
+
 const mongoose = require("mongoose");
 const Dishes = require("./models/dishes");
 const url =
@@ -46,24 +49,21 @@ app.use(
     })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-    if (!req.session.user) {
+    if (!req.user) {
         var err = new Error("You are not authenticated!");
         err.status = 403;
-        return next(err);
+        next(err);
     } else {
-        if (req.session.user === "authenticated") {
-            next();
-        } else {
-            var err = new Error("You are not authenticated!");
-            err.status = 403;
-            return next(err);
-        }
+        next();
     }
 }
 
